90afb8282c035c594d775252e06b2cf6b77bcfca
obaydul_Kuasha <obaydul00000@gmail.com>
2024-05-07 20:37:40 +0600

    update my code pad  then use able my code pade

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -35,14 +35,14 @@ function App() {
 
       {/* <div className="gird grid-flow-row grid-cols-2 "> */}
 
-        <div className=" pane top-pane ">
-          <Editor language="xml" displayName="Html" value={html} onChange={setHtml} />
-          <Editor language="css" displayName="CSS" value={css} onChange={setCss} />
-          <Editor language="javascript" displayName="Javascript" value={javascript} onChange={setJavascript} />
-        </div>
 
         <div className="pane">
           <iframe srcDoc={srcDoc} title="output" sandbox="allow-scripts" frameborder="0" width="100%" height="100%"></iframe>
+        </div>
+        <div className=" pane top-pane font-semibold ">
+          <Editor language="xml" displayName="HTML CODE" value={html} onChange={setHtml} />
+          <Editor language="css" displayName="CSS CODE" value={css} onChange={setCss} />
+          <Editor language="javascript" displayName="JS CODE" value={javascript} onChange={setJavascript} />
         </div>
       {/* </div> */}
     </ >

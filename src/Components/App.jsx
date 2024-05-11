89696238673a7385fa560210b76b8ee69805e944
obaydul_Kuasha <obaydul00000@gmail.com>
2024-05-11 21:49:23 +0600

    this code pad porject complete an i think my butter upgread four futer

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,12 @@
 
 import { useEffect, useState } from 'react';
-
 import Editor from './Editor';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function App() {
-  const [html, setHtml] = useState('');
-  const [css, setCss] = useState('');
-  const [javascript, setJavascript] = useState(' ');
+  const [html, setHtml] = useLocalStorage('html', '');
+  const [css, setCss] = useLocalStorage('css', '');
+  const [javascript, setJavascript] = useLocalStorage('js', ' ');
   const [srcDoc, SetSrcDoc] = useState(' ');
 
   useEffect(() => {
@@ -19,31 +19,22 @@ function App() {
               </html>
               `
       )
-    }, 250)
+    }, 1000)
     return () => clearTimeout(timeout)
   }, [html, css, javascript])
-
-
-
-
-
-
-
 
   return (
     < >
 
       {/* <div className="gird grid-flow-row grid-cols-2 "> */}
-
-
-        <div className="pane">
-          <iframe srcDoc={srcDoc} title="output" sandbox="allow-scripts" frameborder="0" width="100%" height="100%"></iframe>
-        </div>
-        <div className=" pane top-pane font-semibold ">
-          <Editor language="xml" displayName="HTML CODE" value={html} onChange={setHtml} />
-          <Editor language="css" displayName="CSS CODE" value={css} onChange={setCss} />
-          <Editor language="javascript" displayName="JS CODE" value={javascript} onChange={setJavascript} />
-        </div>
+      <div className="pane">
+        <iframe srcDoc={srcDoc} title="output" sandbox="allow-scripts" frameborder="0" width="100%" height="100%"></iframe>
+      </div>
+      <div className=" pane top-pane font-semibold ">
+        <Editor language="xml" displayName="HTML CODE" value={html} onChange={setHtml} />
+        <Editor language="css" displayName="CSS CODE" value={css} onChange={setCss} />
+        <Editor language="javascript" displayName="JS CODE" value={javascript} onChange={setJavascript} />
+      </div>
       {/* </div> */}
     </ >
   );
--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
-import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { Controlled as ControlledEditor } from 'react-codemirror2' 
+import FontAwesome from 'react-fontawesome';
+import { FaCompressAlt, FaExpandAlt } from 'react-icons/fa';
 
 export default function Editor(props) {
     const { language, displayName, value, onChange } = props
+    const [open, setOpen] = useState(true);
+
+     
+
+
     function handleChange(editor, data, value) {
         onChange(value)
     }
     return (
-        <div className="editor-container">
+        <div className={`editor-container ${open ? '' : 'collapsed'}`}>
             <div className=" flex justify-between text-white bg-slate-500 p-2 rounded">
                 {displayName}
-                <button className=' rounded bg-white text-black px-2'>O/C</button>
+                <button type='button' className=' ml-5 bg-none border-none text-white cursor-pointer ' onClick={() => setOpen(prevOpen => !prevOpen)} className=' rounded bg-white text-black px-2 flex-grow-0'>
+                    <FontAwesome icon={open ? FaCompressAlt : FaExpandAlt} ></FontAwesome>
+                </button>
             </div>
             <ControlledEditor
                 onBeforeChange={handleChange}
                 value={value}
                 className='code-mirror-wrapper'
-                options={{  lineWrapping: true, lint: true, mode: language, theme: 'material', lineNumbers: true,
+                options={{
+                    lineWrapping: true, lint: true, mode: language, theme: 'material', lineNumbers: true,
                 }}
             />
         </div>
